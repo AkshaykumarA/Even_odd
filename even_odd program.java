2038891a26a1b2a4ef6dfdf00ea09f5528352ded
AkshaykumarA <akshaykumar1211@gmail.com>
2016-06-23 21:39:42 +0530

    even_odd program.java
    Used to find whether a number is even or odd.

---
+++ even_odd program.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Number {
+
+	public static void main(String[] args) {
+    Scanner a=new Scanner(System.in);
+		System.out.println("Enter the number");
+		int number=a.nextInt();  
+		
+		if(number%2==0){
+			System.out.println("The number is even");}
+			else
+			{
+				System.out.println("The number is odd");
+			}
+		
+	}
+
+}
